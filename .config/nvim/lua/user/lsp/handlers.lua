6d712337b97d9af19d3f9d2970cde002c2c06578
Leo sun <tomyail1989@gmail.com>
2022-06-05 17:38:39 +0800

    新增命令可以在保存时自动格式化

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
 -- TODO: backfill this to template
 M.setup = function()
   local signs = {
@@ -77,20 +78,46 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+  local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+  if not status_cmp_ok then
+    return
   end
+
+  M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+  M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-  return
+function M.enable_format_on_save()
+  vim.cmd [[
+    augroup format_on_save
+      autocmd! 
+      autocmd BufWritePre * lua vim.lsp.buf.formatting()
+    augroup end
+  ]]
+  vim.notify "Enabled format on save"
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+function M.disable_format_on_save()
+  M.remove_augroup "format_on_save"
+  vim.notify "Disabled format on save"
+end
+
+function M.toggle_format_on_save()
+  if vim.fn.exists "#format_on_save#BufWritePre" == 0 then
+    M.enable_format_on_save()
+  else
+    M.disable_format_on_save()
+  end
+end
+
+function M.remove_augroup(name)
+  if vim.fn.exists("#" .. name) == 1 then
+    vim.cmd("au! " .. name)
+  end
+end
+-- 注册一个自定义命令 LspToggleAutoFormat
+vim.cmd [[ command! LspToggleAutoFormat execute 'lua require("user.lsp.handlers").toggle_format_on_save()' ]]
 
 return M
