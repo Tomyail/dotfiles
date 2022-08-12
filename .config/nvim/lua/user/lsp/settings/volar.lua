c15bdc1300fc3da47b75138ade24d0b5ddf71777
lixuexin03 <lixuexin03@meituan.com>
2022-08-12 14:49:32 +0800

    add global vetur config

--- .config/nvim/lua/user/lsp/settings/volar.lua
+++ .config/nvim/lua/user/lsp/settings/volar.lua
@@ -1,4 +1,39 @@
-
 return {
-  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+	documentFeatures = {
+		documentColor = false,
+		documentFormatting = {
+			defaultPrintWidth = 100,
+		},
+		documentSymbol = true,
+		foldingRange = true,
+		linkedEditingRange = true,
+		selectionRange = true,
+	},
+	languageFeatures = {
+		callHierarchy = true,
+		codeAction = true,
+		codeLens = true,
+		completion = {
+			defaultAttrNameCase = "kebabCase",
+			defaultTagNameCase = "both",
+		},
+		definition = true,
+		diagnostics = true,
+		documentHighlight = true,
+		documentLink = true,
+		hover = true,
+		implementation = true,
+		references = true,
+		rename = true,
+		renameFileRefactoring = true,
+		schemaRequestService = true,
+		semanticTokens = false,
+		signatureHelp = true,
+		typeDefinition = true,
+	},
+	typescript = {
+		serverPath = "",
+	},
+	ignoreProjectWarning = true,
 }
